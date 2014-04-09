b5dc852909b5a372e86a465bc539ab6704416c7d
Maxwell Dergosits <mad293@cornell.edu>
2014-04-08 21:35:31 -0400

    commented the example

--- example/mlog_example.go
+++ example/mlog_example.go
@@ -6,19 +6,19 @@ import "mlog"
 func main() {
 
 
-  levels := []string{"debug","network","general"}
+  levels := []string{"debug","network","general"} // These are the enabled levels
 
-  log := mlog.Create(levels,"",true,false)
+  log := mlog.Create(levels,"",true,false) // create logging system that disables the file and enables the levels in the slice above
 
   log.VPrintln("debug","This should Print")
   log.VPrintln("debug2","This should not Print")
   log.VPrintln("network","This should Print")
   log.NPrintln("This should Print")
 
-  log.NPrintln("NEW TEST")
+  log.NPrintln("changing the logger!") // we have a new logger after this println
   levels = []string{}
 
-  log = mlog.Create(levels,"",true,true)
+  log = mlog.Create(levels,"",true,true) // create logging systems that prints all verbose and normal messages
 
   log.VPrintln("debug","This should Print")
   log.VPrintln("debug2","This should Print")
